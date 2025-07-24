ed98dc8cf94425cb5124249a301492c94acfe5f9
kurkusha <nikitavakuluk54@gmail.com>
2025-07-24 22:54:16 +0300

    inputSystem

--- Assets/scripts/Player/Player.cs
+++ Assets/scripts/Player/Player.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    public float movingspeed = 5f;  
+
+    public Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+     
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        inputVector = inputVector.normalized;
+        rb.MovePosition(rb.position + inputVector * (movingspeed * Time.fixedDeltaTime));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
+
 }
